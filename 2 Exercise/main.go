862d015e07435ff008ed6d213b1d36069631cb84
jaenmo <103098534+jaenmo@users.noreply.github.com>
2022-07-04 16:31:07 +0100

    Add files via upload
    Exercise 2

---
+++ 2 Exercise/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	pebbleWeight := 0.1
+	rockWeight := 1.2
+	boulderWeight := 502.4
+	totalWeight := pebbleWeight + rockWeight + boulderWeight
+	fmt.Println(totalWeight)
+}
