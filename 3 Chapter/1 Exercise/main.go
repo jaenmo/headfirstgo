1dd726d92b3596a860d15a6456563d26f74ef497
jaenmo <j.enjuto.monje@gmail.com>
2022-07-05 22:37:55 +0100

    working on the exercise 3

--- 3 Chapter/1 Exercise/main.go
+++ 3 Chapter/1 Exercise/main.go
@@ -5,11 +5,28 @@ import "fmt"
 // YOUR CODE HERE: Write a scoreSummary function that will
 // work with the calls below.
 
-func main() {
-	fmt.Printf("%10s | %8s | %8s | %8s | %8s\n",
-		"Name", "Score 1", "Score 2", "Score 3", "Average")
-	fmt.Println("------------------------------------------------------")
-	scoreSummary("Jermaine", 95.4, 82.3, 74.6)
-	scoreSummary("Jessie", 79.3, 99.1, 82.5)
-	scoreSummary("Lamar", 82.2, 95.4, 77.6)
-}``
+
+// func main() {
+// 	fmt.Printf("%10s | %8s | %8s | %8s | %8s\n",
+// 		"Name", "Score 1", "Score 2", "Score 3", "Average")
+// 	fmt.Println("------------------------------------------------------")
+// 	scoreSummary("Jermaine", 95.4, 82.3, 74.6)
+// 	scoreSummary("Jessie", 79.3, 99.1, 82.5)
+// 	scoreSummary("Lamar", 82.2, 95.4, 77.6)
+// }
+
+	
+// 	func scoreSummary(a string, b float64, c float64, d float64) {
+// 		e := (b+c+d)/3
+		
+// 	fmt.Printf("%10s | %8s | %8s | %8s | %8s\n",
+// 		 a, b, c, d, e)
+
+// 	}
+
+func main(){
+	fmt.Println(greet("Jane ", "Doe "))
+}
+func greet(fname, lname string) string{
+	return fmt.Sprint(fname, lname)
+}
