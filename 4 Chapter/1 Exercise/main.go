edc106e947372e1ad57bbbd7355b83c34458757c
jaenmo <j.enjuto.monje@gmail.com>
2022-07-17 20:18:38 +0100

    update

--- 4 Chapter/1 Exercise/main.go
+++ 4 Chapter/1 Exercise/main.go
@@ -1,12 +1,13 @@
 package main
 
-import "fmt"
-
-var originalCount, eatenCount int = 10, 4
-
+import ("fmt"
+	"time"
+"strings")
 
 func main (){
-	fmt.Println("I started with", originalCount, "apples.")
-	fmt.Println("Some jerk ate", eatenCount, "apples.")
-	fmt.Println("There are", originalCount-eatenCount, "apples left.")
+
+	var now time.Time = time.Now()
+	var date int = now.Day()
+	fmt.Println(date)
+	
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jaenmo/headfirstgo
+
+go 1.18
