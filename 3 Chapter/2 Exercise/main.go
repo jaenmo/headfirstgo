3e8b9bd0a0b60f9af969dbad477a09e1b76d7df9
jaenmo <j.enjuto.monje@gmail.com>
2022-07-29 19:42:34 +0100

    updating 2 exercise with error

--- 3 Chapter/2 Exercise/main.go
+++ 3 Chapter/2 Exercise/main.go
@@ -13,14 +13,20 @@ func main() {
 	// YOUR CODE HERE: Call "perimeter" three times.
 	// Add the three return values together, and store the
 	// total in the "total" variable.
-
-	a := perimeter(8.2, 10)
-	b := perimeter(5, 5.2)
-	c := perimeter(6.2, 4.5)
+	a, err := perimeter(8.2, 10)
+	if err =!nil {
+		log.Fatal(err)
+	}
+	b, err := perimeter(5, 5.2)
+	c, err := perimeter(6.2, 4.5)
 	total := a + b + c
 
 	fmt.Printf("You'll need %.2f meters of fencing", total)
 }
-func perimeter (length, width float64) float64{
-	return (2*length) + (2*width) 
+func perimeter (length, width float64) (float64, error){
+	if length >0 && width >0{
+	return (2*length) + (2*width), nil
+	}else{
+		return "can't print it"
+	}
 }
