64787f350d446a317ceb026e05c4cecf9af1bcd9
jaenmo <j.enjuto.monje@gmail.com>
2022-07-29 22:37:05 +0100

    updated errors

--- 3 Chapter/2 Exercise/main.go
+++ 3 Chapter/2 Exercise/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 // YOUR CODE HERE: Write a "perimeter" function.
 
@@ -13,20 +16,29 @@ func main() {
 	// YOUR CODE HERE: Call "perimeter" three times.
 	// Add the three return values together, and store the
 	// total in the "total" variable.
+
 	a, err := perimeter(8.2, 10)
-	if err =!nil {
+	if err != nil {
 		log.Fatal(err)
 	}
 	b, err := perimeter(5, 5.2)
-	c, err := perimeter(6.2, 4.5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c, err := perimeter(6.2, -4.5)
+	if err != nil {
+		log.Fatal(err)
+	}
 	total := a + b + c
 
 	fmt.Printf("You'll need %.2f meters of fencing", total)
 }
 func perimeter (length, width float64) (float64, error){
-	if length >0 && width >0{
-	return (2*length) + (2*width), nil
-	}else{
-		return "can't print it"
+	if length <0 {
+	return 0, fmt.Errorf("a length of %.2f is invalid\n", length)
 	}
+	if  width <0{
+		return 0, fmt.Errorf("a width of %.2f is invalid\n", width)
+	}
+	return (2*length) + (2*width), nil
 }
