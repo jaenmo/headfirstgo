8a42398dd981d27bad36fca1d407f3e5a05916cd
jaenmo <j.enjuto.monje@gmail.com>
2022-07-30 20:36:39 +0100

    practicing with arrays

---
+++ 4 Chapter/3 Exercise/main.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+func main(){
+	ar := [3]int{1, 2, 3}
+
+	fmt.Println(ar)
+	ar[2] = 0
+	fmt.Println(ar)
+
+	notes := [5]int{11, 22, 33, 44, 55}
+	fmt.Println(len(notes))
+
+	for i:=0; i<(len(notes)); i++{
+		fmt.Println(i, notes[i])
+	}
+	fmt.Println(len(ar))
+}
