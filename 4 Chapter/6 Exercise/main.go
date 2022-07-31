649f3898cbda716e6c3b00a9672088c254bf6370
jaenmo <j.enjuto.monje@gmail.com>
2022-07-31 15:34:16 +0100

    number exercise

---
+++ 4 Chapter/6 Exercise/main.go
@@ -0,0 +1,29 @@
+// create an array with the day of the week and print them with the index
+
+package main
+
+import "fmt"
+
+func main() {
+	// We'll count the number of times each number occurs
+	// within this array.
+	numbers := [9]int{1, 0, 2, 0, 1, 0, 0, 1, 2}
+	// YOUR CODE HERE: Process each element of "numbers",
+	// keeping track of how many times you've seen 0, 1,
+	// or 2.
+	// Finally, print out how many times each number
+	// occurred.
+
+	ocurrences := [3]int{}
+
+	for _, number := range numbers{
+		ocurrences [number] ++
+	}
+	for number, count := range ocurrences{
+		fmt.Println(number, count)
+	}
+}
+
+
+
+
