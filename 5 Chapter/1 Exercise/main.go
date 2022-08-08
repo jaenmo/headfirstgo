3080a766a33e0709a4bf579f06b354104dc1cc0b
jaenmo <j.enjuto.monje@gmail.com>
2022-08-08 08:17:50 +0100

    add

---
+++ 5 Chapter/1 Exercise/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+	candidates := []string{"Grahan Bell","Grahan Bell", "Grahan Bell", "Grahan Bell",
+	 "Brian Martin", "Brian Martin", "Brian Martin", "Brian Martin", "Brian Martin", "Brian Martin",
+	  "Carlos Diaz", "Carlos Diaz", "Carlos Diaz", "Carlos Diaz"}
+
+	//var ocurrences [] int
+	//var sum int
+
+	for i, name := range candidates{
+		//ocurrences[i]++
+		fmt.Println(i,name)
+
+	}
+	// for number, count := range ocurrences{
+	// 	fmt.Println("The candidate", number, "is repeated", count, "times")
+	// 	sum += count
+	// }
+	//fmt.Println(sum)
+	add
+}
