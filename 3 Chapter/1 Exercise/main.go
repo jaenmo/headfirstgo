1960fa4f224bb11caab0fcb06ad76d863b58a333
jaenmo <j.enjuto.monje@gmail.com>
2022-09-11 17:39:17 +0100

    sc

--- 3 Chapter/1 Exercise/main.go
+++ 3 Chapter/1 Exercise/main.go
@@ -1,10 +1,9 @@
 package main
 
-import "fmt"
-
+import ("fmt"
+		"time")
 // YOUR CODE HERE: Write a scoreSummary function that will
 // work with the calls below.
-
 
 func main() {
 	fmt.Printf("%10s | %8s | %8s | %8s | %8s\n",
@@ -13,13 +12,21 @@ func main() {
 	scoreSummary("Jermaine", 95.4, 82.3, 74.6)
 	scoreSummary("Jessie", 79.3, 99.1, 82.5)
 	scoreSummary("Lamar", 82.2, 95.4, 77.6)
+
+	var l float64 = 2.2
+	var le int = 5
+	fmt.Println(int(l) + le)
+
+	ast := time.Now()
+	year := ast.Year()
+	fmt.Println(year)
 }
 
-	func scoreSummary(a string, b float64, c float64, d float64) {
-		e := (b+c+d)/3
-	
-		fmt.Printf("%10s | %8.2f | %8.2f | %8.2f | %8.2f\n",
-		 a, b, c, d, e)
+func scoreSummary(a string, b float64, c float64, d float64) {
+	e := (b + c + d) / 3
 
-	}
-wftafccfzz
+	fmt.Printf("%10s | %8.2f | %8.2f | %8.2f | %8.2f\n",
+		a, b, c, d, e)
+
+	asa	
+}
